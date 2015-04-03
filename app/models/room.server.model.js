fd6d673395aa5969c2fb4e38930d39360257a9ae
Alexis Paques <alexis.paques@gmail.com>
2015-04-03 15:51:39 +0200

    Correction de l'indentation

--- app/models/room.server.model.js
+++ app/models/room.server.model.js
@@ -4,38 +4,38 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 /**
  * Room Schema
  */
 var RoomSchema = new Schema({
-	ID: {
+  ID: {
     type:String,
     default:'0A00',
     required: 'ID cannot be null',
     trim: true
   },
   name: {
-		type: String,
-		default: '',
-		required: 'Please fill Room name',
-		trim: true
-	},
+    type: String,
+    default: '',
+    required: 'Please fill Room name',
+    trim: true
+  },
   seats: {
     type:Number,
     default:0,
     required: 'There must be seats',
     trim: true
   },
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
 });
 
 mongoose.model('Room', RoomSchema);
