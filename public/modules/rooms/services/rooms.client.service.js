c4eed5b8f30d3888114f1e29ed1c1d8116a2852d
Alexis Paques <alexis.paques@gmail.com>
2015-04-03 17:10:23 +0200

    Modifying URL

--- public/modules/rooms/services/rooms.client.service.js
+++ public/modules/rooms/services/rooms.client.service.js
@@ -3,7 +3,7 @@
 //Rooms service used to communicate Rooms REST endpoints
 angular.module('rooms').factory('Rooms', ['$resource',
 	function($resource) {
-		return $resource('rooms/:roomId', { roomId: '@ID'
+		return $resource('rooms/:roomId', { roomId: '@_id'
 		}, {
 			update: {
 				method: 'PUT'
