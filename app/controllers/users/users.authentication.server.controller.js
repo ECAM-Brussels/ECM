00ea089cc712c650ba353b05d0a37cf80df0a638
Alexis Paques <alexis.paques@gmail.com>
2015-04-29 08:46:48 +0200

    Signup with a serial, not a username

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -19,6 +19,7 @@ exports.signup = function(req, res) {
   // Add missing user fields
   user.provider = 'local';
   user.displayName = user.firstName + ' ' + user.lastName;
+  user.username = user.serial;
   // Then save the user 
   user.save(function(err) {
     if (err) {
