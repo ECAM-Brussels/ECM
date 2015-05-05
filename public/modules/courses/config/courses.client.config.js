bb7bce636432e9660b9131a14fb21e791d304c89
Sébastien Combéfis <seb478@gmail.com>
2015-05-05 08:19:50 +0200

    Update view of list of courses
    - Added the list of activities for each course

--- public/modules/courses/config/courses.client.config.js
+++ public/modules/courses/config/courses.client.config.js
@@ -7,6 +7,6 @@ angular.module( 'courses' ).run( [ 'Menus',
     Menus.addMenuItem( 'topbar', 'Courses', 'courses', 'dropdown', '/courses(/create)?', false, [ 'admin', 'manager', 'teacher' ] );
     Menus.addSubMenuItem( 'topbar', 'courses', 'List Courses', 'courses', 'courses', false, [ 'admin', 'manager' ] );
     Menus.addSubMenuItem( 'topbar', 'courses', 'New Course', 'courses/create', 'courses/create', false, [ 'admin', 'manager' ] );
-    Menus.addSubMenuItem( 'topbar', 'courses', 'My Courses', 'list/myCourses', 'list/myCourses', false, [ 'teacher' ] );
+    Menus.addSubMenuItem( 'topbar', 'courses', 'List', 'list/myCourses', 'list/myCourses', false, [ 'teacher' ] );
  }
  ] );
