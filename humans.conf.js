edc739019588c5b271bd7e8f027c832a7b028ba8
Alexis Paques <alexis.paques@gmail.com>
2015-05-11 13:26:57 +0200

    humans.txt grunt task

---
+++ humans.conf.js
@@ -0,0 +1,22 @@
+{
+  "header": "ECM  ECAM",
+  "team": {
+    "Paques Alexis": {
+      "Email": "alexis.paques@gmail.com",
+      "Country": "Brussels",
+      "Organization": "ECAM, CSited"
+    },
+    "Combéfis Sébastien": {
+      "Email": "cbf@ecam.be",
+      "Country": "Brussels",
+      "Organization": "ECAM, CSited"
+    }
+  },
+  "thanks": [ "Lurkin Quentin <lur@ecam.be>" ],
+  "site": {
+    "Standards": "HTML5, CSS3",
+    "Components": "AngularJS, MongoDB, Papaparse, Express, NodeJS, Bootstrap, jQuery",
+    "Software": "Sublime Text"
+  },
+  "out": "public/humans.txt"
+}
