9db3d491df75a28c877f2fc35951227d8d3081d9
Sébastien Combéfis <seb478@gmail.com>
2015-05-11 15:24:00 +0200

    Change menu name

--- public/modules/courses/config/courses.client.config.js
+++ public/modules/courses/config/courses.client.config.js
@@ -6,5 +6,5 @@ angular.module('courses').run(['Menus', function(Menus) {
 	Menus.addMenuItem('topbar', 'Courses', 'courses', 'dropdown', '/courses(/create)?', false, ['admin', 'manager', 'teacher']);
 	Menus.addSubMenuItem('topbar', 'courses', 'List', 'courses', 'courses', false, ['admin', 'manager']);
 	Menus.addSubMenuItem('topbar', 'courses', 'Create', 'courses/create', 'courses/create', false, ['admin']);
-	Menus.addSubMenuItem('topbar', 'courses', 'List', 'list/myCourses', 'list/myCourses', false, ['teacher']);
+	Menus.addSubMenuItem('topbar', 'courses', 'My courses', 'list/myCourses', 'list/myCourses', false, ['teacher']);
 }]);
