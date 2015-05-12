2288a8ed0f045d8fa0e7f185c6bc918ee1ceefb7
Sébastien Combéfis <seb478@gmail.com>
2015-05-12 09:28:14 +0200

    Update user management
    - Changed the style of the views to agree with other pages

--- public/modules/users/controllers/users.list.client.controller.js
+++ public/modules/users/controllers/users.list.client.controller.js
@@ -1,67 +1,61 @@
 'use strict';
-angular.module( 'users' ).controller( 'UsersController', [ '$scope', '$stateParams', '$http', '$location', 'Authentication', 'Users',
-  function ( $scope, $stateParams, $http, $location, Authentication, Users ) {
-    $scope.authentication = Authentication;
+angular.module( 'users' ).controller( 'UsersController', [ '$scope', '$stateParams', '$http', '$location', 'Authentication', 'Users', function ( $scope, $stateParams, $http, $location, Authentication, Users) {
+	$scope.authentication = Authentication;
 
-    $scope.find = function () {
-      $scope.users = Users.query();
-    };
+	$scope.find = function() {
+		$scope.users = Users.query();
+	};
 
-    $scope.createUser = function () {
-      $http.post( '/users', $scope.credentials ).success( function ( response ) {
-        $location.path( '/users' );
-      } ).error( function ( response ) {
-        $scope.error = response.message;
-      } );
-    };
+	$scope.createUser = function () {
+		$http.post('/users', $scope.credentials).success(function(response) {
+			$location.path('/users');
+		}).error(function(response) {
+			$scope.error = response.message;
+		});
+	};
 
+	$scope.updateUser = function() {
+		var user = $scope.profile;
+		user.$update(function() {
+			$location.path('users/' + user.serial);
+		}, function(errorResponse) {
+			$scope.error = errorResponse.data.message;
+		});
+	};
+	
+	// Find existing user
+	$scope.findOne = function() {
+		var user = Users.get({
+			serial: $stateParams.serial
+		}, function() {
+			user.rights = {
+				manager: exists('manager', user.roles),
+				admin: exists('admin', user.roles),
+				user: exists('user', user.roles),
+				teacher: exists('teacher', user.roles),
+				printer: exists('printer', user.roles)
+			};
+			$scope.profile = user;
+		});
+	};
 
-    $scope.updateUser = function() {
-      var user = $scope.profile;
+	var exists = function(val, array) {
+		return array.indexOf(val) > -1;
+	};
 
-      user.$update(function() {
-        $location.path('users/' + user.serial);
-      }, function(errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
-    // Find existing user
-    $scope.findOne = function () {
-      var u = Users.get( {
-        serial: $stateParams.serial
-      }, function () {
-        u.rights = {
-          manager: exists( 'manager', u.roles ),
-          admin: exists( 'admin', u.roles ),
-          user: exists( 'user', u.roles ),
-          teacher: exists( 'teacher', u.roles ),
-          printer: exists( 'printer', u.roles )
-        };
-        $scope.profile = u;
-      } );
-    };
-
-    var exists = function ( val, array ) {
-      if ( array.indexOf( val ) > -1 ) return true;
-      return false;
-    };
-
-    // Find existing user
-    $scope.remove = function ( profile ) {
-      if ( profile ) {
-        profile.$remove();
-
-        for ( var i in $scope.users ) {
-          if ( $scope.users[ i ] === profile ) {
-            $scope.users.splice( i, 1 );
-          }
-        }
-      } else {
-        $scope.profile.$remove( function () {
-          $location.path( 'users' );
-        } );
-      }
-    };
-
-  }
- ] );
+	// Find existing user
+	$scope.remove = function(profile) {
+		if (profile) {
+			profile.$remove();
+			for (var i in $scope.users) {
+				if ($scope.users[i] === profile) {
+					$scope.users.splice(i, 1);
+				}
+			}
+		} else {
+			$scope.profile.$remove(function() {
+				$location.path('users');
+			});
+		}
+	};
+}]);
