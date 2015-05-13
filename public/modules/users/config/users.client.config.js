40a0b8dc7cd4afb05c4ff4354dcbdacf90b85222
Sébastien Combéfis <seb478@gmail.com>
2015-05-13 10:17:59 +0200

    Change rights for "Users" and "Groups" menus
    - Both menus are now not available to manager

--- public/modules/users/config/users.client.config.js
+++ public/modules/users/config/users.client.config.js
@@ -1,40 +1,36 @@
 'use strict';
 
 // Config HTTP Error Handling
-angular.module( 'users' ).config( [ '$httpProvider',
- function ( $httpProvider ) {
-    // Set the httpProvider "not authorized" interceptor
-    $httpProvider.interceptors.push( [ '$q', '$location', 'Authentication',
-   function ( $q, $location, Authentication ) {
-        return {
-          responseError: function ( rejection ) {
-            switch ( rejection.status ) {
-            case 401:
-              // Deauthenticate the global user
-              Authentication.user = null;
+angular.module('users').config(['$httpProvider', function($httpProvider) {
+	// Set the httpProvider "not authorized" interceptor
+	$httpProvider.interceptors.push(['$q', '$location', 'Authentication', function($q, $location, Authentication) {
+		return {
+			responseError: function (rejection) {
+				switch (rejection.status) {
+					case 401:
+						// Deauthenticate the global user
+						Authentication.user = null;
 
-              // Redirect to signin page
-              $location.path( 'signin' );
-              break;
-            case 403:
-              // Add unauthorized behaviour 
-              break;
-            }
+						// Redirect to signin page
+						$location.path('signin');
+					break;
 
-            return $q.reject( rejection );
-          }
-        };
-   }
-  ] );
- }
- ] );
+					case 403:
+						// Add unauthorized behaviour 
+					break;
+				}
+				return $q.reject(rejection);
+			}
+		};
+	}]);
+}]);
 
 // Users edit module
-angular.module( 'students' ).run( [ 'Menus',
-  function ( Menus ) {
-    // Set top bar menu items
-    Menus.addMenuItem( 'topbar', 'Users', 'users', 'dropdown', '/users(/create)?', false, [ 'admin', 'manager' ] );
-    Menus.addSubMenuItem( 'topbar', 'users', 'List', 'users', 'users', false, [ 'admin', 'manager' ] );
-    Menus.addSubMenuItem( 'topbar', 'users', 'Create', 'users/create', 'users/create', false, [ 'admin', 'manager' ] );
-  }
- ] );
+angular.module('students').run(['Menus', function(Menus) {
+	var canedit = ['admin'];
+
+	// Set top bar menu items
+	Menus.addMenuItem('topbar', 'Users', 'users', 'dropdown', '/users(/create)?', false, canedit);
+	Menus.addSubMenuItem('topbar', 'users', 'List', 'users', 'users', false, canedit);
+	Menus.addSubMenuItem('topbar', 'users', 'Create', 'users/create', 'users/create', false, canedit);
+}]);
